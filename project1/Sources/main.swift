00fb92a4ccc9bee14401c1bb26dddff1e6611d96
cojoj <zajac.mateusz@icloud.com>
2016-12-14 13:50:08 +0100

    Milion hairs

--- project1/Sources/main.swift
+++ project1/Sources/main.swift
@@ -1,8 +1,24 @@
 import Kitura
 import HeliumLogger
+import LoggerAPI
 
 HeliumLogger.use()
 let router = Router()
+
+router.get("/") { request, response, next in
+    response.send("Welcome to Million Hairs")
+    next()
+}
+
+router.get("/staff") { request, response, next in
+    response.send("Meet our great team")
+    next()
+}
+
+router.get("/contact") { request, response, next in
+    response.send("Get in touch with us")
+    next()
+}
 
 Kitura.addHTTPServer(onPort: 8090, with: router)
 Kitura.run()
