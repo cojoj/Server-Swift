3e74c7f463e45a4677881163f4d8ca13a75000c2
cojoj <zajac.mateusz@icloud.com>
2016-12-14 14:46:12 +0100

    Stencil inheritance

--- project1/Sources/main.swift
+++ project1/Sources/main.swift
@@ -6,6 +6,7 @@ import KituraStencil
 HeliumLogger.use()
 let router = Router()
 router.setDefault(templateEngine: StencilTemplateEngine())
+router.all("/static", middleware: StaticFileServer())
 
 router.get("/") { request, response, next in
     defer { next() }
@@ -17,9 +18,30 @@ router.get("/contact") { request, response, next in
     try response.render("contact", context: [:])
 }
 
-router.get("/staff") { request, response, next in
-    response.send("Meet our great team")
-    next()
+router.get("/staff/:name") { request, response, next in
+    defer { next() }
+    
+    guard let name = request.parameters["name"] else {
+        return
+    }
+    
+    let bios = [
+        "kirk": "My name is James Kirk and I love snakes.",
+        "picard": "My name is Jean-Luc Picard and I'm mad for cats.",
+        "sisko": "My name is Benjamin Sisko and I'm all about the budgies.",
+        "janeway": "My name is Kathryn Janeway and I want to hug every hamster.",
+        "archer": "My name is Jonathan Archer and beagles are mything."
+    ]
+    
+    var context = [String: Any]()
+    context["people"] = bios.keys.sorted()
+    
+    if let bio = bios[name] {
+        context["name"] = name
+        context["bio"] = bio
+    }
+    
+    try response.render("staff", context: context)
 }
 
 Kitura.addHTTPServer(onPort: 8090, with: router)
